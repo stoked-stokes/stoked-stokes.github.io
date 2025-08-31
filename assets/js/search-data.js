513e8c4de5fb02b3c46a9c525495fd41b51c6eb3
stoked-stokes <96992319+stoked-stokes@users.noreply.github.com>
2025-08-31 20:29:59 +0000

    Deploying to gh-pages from @ stoked-stokes/stoked-stokes.github.io@8b9c9eb7857e0555598cda4ca0868b8c21ce62c9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,14 +431,11 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+    },{id: "news-first-paper-up-on-arxiv-check-it-out-here",
+          title: 'First paper up on arxiv! Check it out here.',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/news/announcement_1/";
             },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
